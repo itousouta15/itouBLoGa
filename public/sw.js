0ed4da50d1553f95f5e09339481dd096525461b7
itousouta15 <kuoray333@gmail.com>
2026-02-13 21:10:27 +0800

    Regenerate public folder

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1770987248252";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1770988226208";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
